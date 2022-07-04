eae39ffd7edf0f3718b2e072547d0e194d9becde
Eric Garcia <78274657+Egr711@users.noreply.github.com>
2022-07-04 16:05:46 -0500

    Add files via upload

--- separator.py
+++ separator.py
@@ -3,19 +3,30 @@ import math
 import os
 # CHANGE THESE VALUES TO YOUR LIKING
 
-# a = 97
-# f = 102
-# d = 100
 saveCropped = ord('f')
 cancelCropped = ord('a')
 goToNextImage = ord('d')
+rotate = ord('r')
 
 # Shows image in this size
 # (still uses original image size when cropping, this just makes larger images easier to crop)
 height = 900
 
+countStart = 68
+
 
 #========================================#
+
+def getRotationAmount(degrees):
+    if currentRotation == 90:
+        return cv2.cv2.ROTATE_90_CLOCKWISE
+    elif currentRotation == 180:
+        return cv2.cv2.ROTATE_180
+    elif currentRotation == 270:
+        return cv2.cv2.ROTATE_90_COUNTERCLOCKWISE
+    else:
+        return -1
+
 
 def ResizeWithAspectRatio(_image, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
@@ -40,6 +51,7 @@ def shape_selection(event, x, y, flags, param):
     global image
     global currentCroppedImage
     global currentImageName
+    global currentRotation
 
     # Storing the (x1,y1) coordinates when left mouse button is pressed
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -66,13 +78,15 @@ def shape_selection(event, x, y, flags, param):
 
         if xcoord2 == xcoord1 or ycoord1 == ycoord2:
             return
+        currentName = currentImageName + '_' + str(currentCroppedImage - 1) + '.jpg'
+        if countStart != -1:
+            currentName = str(countStart) + '_' + str(currentCroppedImage - 1) + '.jpg'
 
-        # Drawing a rectangle around the region of interest (roi)
-        cv2.imshow(currentImageName + '_' + str(currentCroppedImage) + '.jpg', scaledImage[xcoord1:xcoord2, ycoord1:ycoord2])
+        cv2.imshow(currentName, scaledImage[xcoord1:xcoord2, ycoord1:ycoord2])
         while (1):
             k = cv2.waitKey(33)
             if k == cancelCropped:  # a key
-                cv2.destroyWindow(currentImageName + '_' + str(currentCroppedImage) + '.jpg')
+                cv2.destroyWindow(currentName)
                 break
             elif k == saveCropped: # f key
                 ycoord1 = round(min(coordinates[0][0], coordinates[1][0]) * scaledAmount)
@@ -89,18 +103,43 @@ def shape_selection(event, x, y, flags, param):
                     xcoord2 = xmax
                 if ycoord2 > ymax:
                     ycoord2 = ymax
-                cv2.imwrite('./cropped/' + currentImageName + '_' + str(currentCroppedImage) + '.jpg', image[xcoord1:xcoord2, ycoord1:ycoord2])
+
+                rotatedImage = image[xcoord1:xcoord2, ycoord1:ycoord2]
+                rotationAmount = getRotationAmount(currentRotation)
+                if rotationAmount != -1:
+                    rotatedImage = cv2.rotate(rotatedImage, rotationAmount)
+
+                saveName = currentName
+                if countStart != -1:
+                    currentImageName = currentImageName.split('.jpg')[0]
+                    saveName = str(countStart) + '(' + currentImageName + ')' + '_' + str(currentCroppedImage - 1) + '.jpg'
+                cv2.imwrite('./cropped/' + saveName, rotatedImage)
                 currentCroppedImage += 1
-                cv2.destroyWindow(currentImageName + '_' + str(currentCroppedImage - 1) + '.jpg')
+                cv2.destroyWindow(currentName)
+                currentRotation = 0
                 break
+            elif k == rotate:
+                currentRotation = (currentRotation + 90) % 360
+                rotateAmount = getRotationAmount(currentRotation)
+
+                rotatedImage = scaledImage[xcoord1:xcoord2, ycoord1:ycoord2]
+                if rotateAmount != -1:
+                    rotatedImage = cv2.rotate(rotatedImage, rotateAmount)
+                cv2.destroyWindow(currentName)
+                cv2.imshow(currentName, rotatedImage)
             elif k == -1:
                 continue
             else:
-                print("key pressed:", k)
+                print("cancel:", cancelCropped)
+                print("accept:", saveCropped)
+                print('rotate:', rotate)
+                print("key pressed 2:", k)
         coordinates = []
         scaledImage = ResizeWithAspectRatio(image, height=height)
 
+
 coordinates = []
+currentRotation = 0
 
 listOfImageNames = os.listdir('./images')
 
@@ -119,10 +158,13 @@ for imageName in listOfImageNames :
         while (1):
             k = cv2.waitKey(33)
             if k == goToNextImage:  # d key
+                if countStart != -1:
+                    countStart += 1
                 break
             elif k == -1:
                 continue
             else:
-                print("key pressed:", k)
+                print("go to next:", goToNextImage)
+                print("key pressed 1:", k)
         cv2.destroyWindow(imageName)
         break
